a3e89f744dd7a2e976b47709caa752aadf6a6cc6
Juan122113 <ammiel89@hotmail.com>
2025-04-17 12:47:48 -0300

    Added two name objects

---
+++ main.js
@@ -0,0 +1,5 @@
+function aliasGen(first, last){
+    let firstName = {A: Aim, B: Beto, C: Comrade, D: Dandy, E: Elmo, F: Fuzzy, G: Giddy, H: Happy, I: Icy, J: Jolly, K: Kooky, L: Lucky, M: Merry, N: No, O: Oily, P: Poppy, Q: Quirky, R: Rolly, S: Silly, T: Tasty, U: Unlucky, V: Vain, W: Wacky, X: Xylo, Y: Yummy, Z: Zany};
+
+    let lastName = {A: Apple, B: Banana, C: Cherry, D: Doom, E: Elder, F: Fig, G: Grape, H: Honey, I: Interesting, J: Jack, K: Kiwi, L: Lemon, M: Mango, N: Nectarine, O: Orange, P: Papaya, Q: Quince, R: Raspberry, S: Strawberry, T: Tangerine, U: Ugli, V: Vanilla, W: Watermelon, X: Xigua, Y: Yellow, Z: Zucchini};
+}
