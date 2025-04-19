2179e5907266dd4befd720352d942b12a12caf8a
Juan122113 <ammiel89@hotmail.com>
2025-04-19 15:16:31 -0300

    Test completed & clean code

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 function aliasGen(first, last){
 
-    let firstName = {A: "Aim", B: "Beto", C: "Comrade", D: "Dandy", E: "Elmo", F: "Fuzzy", G: "Giddy", H: "Happy", I: "Icy", J: "Jolly", K: "Kooky", L: "Lucky", M: "Merry", N: "No", O: "Oily", P: "Poppy", Q: "Quirky", R: "Rolly", S: "Silly", T: "Tasty", U: "Unlucky", V: "Vain", W: "Wacky", X: "Xylo", Y: "Yummy", Z: "Zany"};
+    let firstName = {A: "Alpha", B: "Beta", C: "Cache", D: "Data", E: "Energy", F: "Function", G: "Glitch", H: "Half-life", I: "Ice", J: "Java", K: "Keystroke", L: "Logic", M: "Malware", N: "Nagware", O: "OS", P: "Phishing", Q: "Quantum", R: "RAD", S: "Strike", T: "Trojan", U: "Ultraviolet", V: "Vanilla", W: "WiFi", X: "Xerox", Y: "Y", Z: "Zero"};
 
-    let lastName = {A: "Apple", B: "Banana", C: "Cherry", D: "Doom", E: "Elder", F: "Fig", G: "Grape", H: "Honey", I: "Interesting", J: "Jack", K: "Kiwi", L: "Lemon", M: "Mango", N: "Nectarine", O: "Orange", P: "Papaya", Q: "Quince", R: "Raspberry", S: "Strawberry", T: "Tangerine", U: "Ugli", V: "Vanilla", W: "Watermelon", X: "Xigua", Y: "Yellow", Z: "Zucchini"};
+    let lastName = {A: "Analogue", B: "Bomb", C: "Catalyst", D: "Discharge", E: "Electron", F: "Faraday", G: "Gig", H: "Hacker", I: "IP", J: "Jabber", K: "Killer", L: "Lazer", M: "Mike", N: "n00b", O: "Overclock", P: "Payload", Q: "Quark", R: "Roy", S: "Spy", T: "T-Rex", U: "Unit", V: "Virus", W: "Worm", X: "X", Y: "Yob", Z: "Zombie"};
 
-    let firstCharRegex = /^D/i;
+    let firstCharRegex = /\D/;
 
-    let checkingFirstChar = first[0];
+    let firstInitial = first[0].toUpperCase();
+
+    let lastInitial = last[0].toUpperCase();
 
     console.log(firstCharRegex);
+    console.log(firstCharRegex.test(firstInitial));
+    console.log(first[0]);
+    console.log(firstInitial);
+
+    if (firstCharRegex.test(firstInitial) & firstCharRegex.test(lastInitial)) {
+        return firstName[firstInitial] + " " + lastName[lastInitial]
+        
+        
+        
+    } else {
+        return "Your name must start with a letter from A - Z."
+    }
+
+    // 
 
     // if (first[0] )
-
     
         
 }
 
-aliasGen("Ammiel", "Latorre");
+// aliasGen("Ammiel", "Latorre");
+console.log(aliasGen("Malware", "Mike"));
